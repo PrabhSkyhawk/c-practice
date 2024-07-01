0fb9c9da8aa1e53f6339b67360fbfeb76da8e5a7
dell <dell@DESKTOP-ITTGEIV>
2024-07-01 13:13:49 +0530

    Add project files.

---
+++ c#practice/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Runtime.InteropServices;
+// arithemetic operators
+/*
+int sum1 = 20;
+int sum2 = sum1+50; //70
+int sum3 = sum2 + 100;  //170
+Console.WriteLine(sum1);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
+Console.ReadLine();
+*/
+/*
+int x  = 12;
+int y = 3;
+Console.WriteLine(x*y);
+*/
+
+/*
+int x = 15;
+int y = 5;
+Console.WriteLine(x-y);
+Console.ReadLine();
+*/
+/*
+int X = 20;
+int Y = 2;
+Console.WriteLine(X / Y);
+Console.ReadLine();
+*/
+/*
+int x  = 53;
+x++;
+Console.WriteLine(x);
+Console.ReadLine();
+*/
+/*
+int x = 50;
+int y = 7;
+Console.WriteLine(x%y);
+Console.ReadLine();
+*/
+
+// asignment operators
+/*
+int x = 8;
+x += 3;
+Console.WriteLine(x);
+Console.ReadLine(); 
+*/
+/*
+int x = 23;
+x -= 3;
+Console.WriteLine(x);
+Console.ReadLine();
+*/
+/*
+int x = 10;
+x *= 3;
+Console.WriteLine(x);
+Console.ReadLine();
+*/
+/*
+int x = 30;
+x/= 3;
+Console.WriteLine(x);
+Console.ReadLine();
+*/
+/*
+int y = 45;
+y &= 8;
+Console.WriteLine(y);
+Console.ReadLine();
+*/
+/*
+int y = 36;    //2 will be added in 36
+y|= 2;
+Console.WriteLine(y);
+*/
+// comparison operator 
+/*
+int x  =5 ;
+int y =3 ;
+Console.WriteLine(x==y);
+Console.ReadLine(); 
+*/
+/*
+int x = 10;
+int y = 20;
+Console.WriteLine(x>y);
+Console.ReadLine();
+*/
+/*
+int x = 10;
+int y = 20;
+Console.WriteLine(x<y); 
+Console.ReadLine();
+*/
+/*
+int x  = 50;
+int y = 20;
+Console.WriteLine(x>=y);
+Console.ReadLine();
+*/
+/*
+int x = 30;
+int y = 50;
+Console.WriteLine(x <= y);
+Console.ReadLine();
+*/
+//logical operator
+/*
+int x = 20;
+Console.WriteLine(x>5 && x<100);
+Console.ReadLine();
+*/
+/*
+int x = 40;
+Console.WriteLine(x>10 ||x <5);
+Console.ReadLine();
+*/
+int x = 90;
+Console.WriteLine(!(x > 45 && x < 110));
