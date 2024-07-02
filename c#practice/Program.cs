1069d8f6549a5c20dda11cb1ded48fcb6a8e3b68
PRABHJOT SINGH <prabhjotsingh94688@gmail.com>
2024-07-02 11:52:37 +0530

    2 july

--- c#practice/Program.cs
+++ c#practice/Program.cs
@@ -119,5 +119,217 @@ int x = 40;
 Console.WriteLine(x>10 ||x <5);
 Console.ReadLine();
 */
+/*
 int x = 90;
 Console.WriteLine(!(x > 45 && x < 110));
+Console.ReadLine();
+*/
+//c# math
+/*
+Console.WriteLine(Math.Max(5, 15));
+Console.ReadLine();
+Console.WriteLine(Math.Min(5, 15));
+Console.ReadLine();
+*/
+/*
+Console.WriteLine(Math.Sqrt(100));
+Console.ReadLine();
+*/
+// STRING
+/*
+string greeting = "hello";
+Console.WriteLine(greeting);
+Console.ReadLine();
+*/
+/*
+string admission = "present sir";
+Console.WriteLine(admission);
+Console.ReadLine();
+*/
+/*
+string greeting = "hello";
+string greeting2 = "how are you?";
+Console.WriteLine(greeting);    
+Console.WriteLine(greeting2);
+Console.ReadLine();
+*/
+/*
+string txt = "prabhjotsingh";
+Console.WriteLine("The length of the txt string is:" +txt.Length);
+Console.ReadLine();
+*/
+/*
+string txt = "prabhjotsingh";
+string txt2 = "lieutenant";                                                         //from own side
+Console.WriteLine("The length of the txt string is:" + txt.Length);
+Console.WriteLine("The length of the txt2 string is:" + txt2.Length);
+Console.ReadLine();
+*/
+/*
+string txt = "Hello Buddy";
+Console.WriteLine(txt.ToUpper());
+Console.WriteLine(txt.ToLower());
+Console.ReadLine();
+*/
+// string concatenation
+/*
+string FirstName = "prabh";
+string LastName = "singh";
+string Name = FirstName+LastName;
+Console.WriteLine(Name);
+Console.ReadLine(); 
+*/
+/*
+string FirstName = "prabh";
+string LastName = "singh";
+string name = string.Concat(FirstName, LastName);   
+Console.WriteLine(name);    
+Console.ReadLine(); 
+*/
+//Adding numbers and strings 
+/*
+int x = 17;
+int y = 30;
+int z = x + y;
+Console.WriteLine(z);
+*/
+//interpoliation
+/*
+string FirstName = "prabhjot";
+string LastName = "singh";
+string name = $" my name is {FirstName}{LastName}";
+Console.WriteLine(name);
+Console.ReadLine();
+*/
+// access string
+/*
+string greeting="hello";
+Console.WriteLine(greeting[4]);
+Console.ReadLine();
+*/
+// using indexof in access string 
+/*
+string greeting = " hello world";
+Console.WriteLine(greeting.IndexOf("r"));
+Console.ReadLine();
+*/
+/*
+string name = "prabhjot singh";
+int charPos = name.IndexOf("h");
+string lastName=name.Substring(charPos);
+Console.WriteLine(lastName);
+*/
+// special characters string 2july 2024
+/*
+string txt = "Welcome to \"the coding\" ninjas session";
+    Console.WriteLine(txt);
+Console.ReadLine();
+*/
+/*
+string txt = "i don\'t want you to come in my way";
+Console.WriteLine(txt);
+Console.ReadLine();
+*/
+/*
+string txt = "i will go\\gone to market";          //tick the right option 
+Console.WriteLine(txt); 
+Console.ReadLine();
+*/
+//Boolean
+/*
+bool doyoulikecoding = true;
+bool doyoulikeswimming = false;
+Console.WriteLine(doyoulikecoding);
+Console.WriteLine(doyoulikeswimming);
+Console.ReadLine();
+*/
+/*
+int x = 50;
+int y = 90;
+Console.WriteLine(x>y); 
+Console.ReadLine();
+*/
+/*
+int myage = 25;
+int votingage  = 18;
+Console.WriteLine(myage>=votingage);
+Console.ReadLine(); 
+*/
+// boolean if-else
+/*
+int myage = 25;
+int myfriendage = 18;
+if (myage >= myfriendage)
+    Console.WriteLine("i am elder than my friend!");
+else
+    Console.WriteLine("my friend is elder than me");
+Console.ReadLine();
+*/
+//    if-else
+//IF
+/*
+if (1000>580)
+    Console.WriteLine("YES!1000 IS GREATER THAN 580");
+Console.ReadLine(); 
+*/
+/*
+int x = 25;
+int y = 53;
+if (x<y)
+Console.WriteLine("YES!53 IS GREATER THAN 25");
+Console.ReadLine();
+*/
+/*
+int time = 1200;
+if (time > 1200)
+    Console.WriteLine("GOOD AFTERNOON");
+else
+    Console.WriteLine("GOOD MORNING");
+Console.ReadLine(); 
+*/
+/*
+int time = 1400;
+if (time > 1300)
+    Console.WriteLine("GOOD AFTERNOON");
+else if (time < 0900)
+    Console.WriteLine("GOOD MORNING");
+else
+    Console.WriteLine("GOOD DAY");
+Console.ReadLine();
+*/
+//short hand if - else
+/*
+int salary = 20000;
+string result = (salary > 30000)?"enjoy the vaccation." : "you need to spend carefully.";
+Console.WriteLine(result);
+Console.ReadLine();
+*/
+//switch
+/*
+int list = 4;
+switch (list)
+{
+    case 1:
+        Console.WriteLine("Intro");
+        break;
+    case 2:
+        Console.WriteLine("acknowledgement");
+        break;
+    case 3:
+        Console.WriteLine("index");
+        break;
+    case 4:
+        Console.WriteLine("summary");
+        break;
+}
+Console.ReadLine();
+*/
+//LOOP
+// while loop
+int i = 3;
+while (i < 10) 
+{
+    Console.WriteLine(i);
+    i++;
+}
+
